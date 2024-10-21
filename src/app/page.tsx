f48be0abfe4f32f69ce3b9dc5d9642b7b2507a36
papapanda_mbp <rafaeljonathan.work@gmail.com>
2024-10-21 14:49:52 +0700

    update:Landing section base done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 const HomePage = () => {
-	return (
-		<div>
-			<h1>Something Good is Comming</h1>
-			<h2>Don&apos;t miss it</h2>
-			<p>all good things takes time</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Something Good is Comming</h1>
+      <h2>Don&apos;t miss it</h2>
+      <p>all good things takes time</p>
+      <p>it&apos;s really does</p>
+    </div>
+  );
 };
 
 export default HomePage;
